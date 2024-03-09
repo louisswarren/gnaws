6c35914d9c9999c86a930a6c922655bfefa23214
Louis Warren <louis@lsw.nz>
2024-03-09 23:33:46 +1300

    Always kill gnaws when test ends

--- test.bash
+++ test.bash
@@ -3,12 +3,11 @@
 PORT="$(( 1000 + ( $RANDOM % ( 32768 - 1000 ) ) ))"
 
 ./gnaws "$PORT" &
-PID="$!"
+trap "kill $!" EXIT HUP INT QUIT ABRT KILL ALRM TERM
 
 for x in `seq 1000`; do
 	if !(netcat localhost "$PORT" | diff -q response.http -); then
 		echo "Netcat test $x failed"
-		kill "$PID"
 		exit 1
 	fi
 done
@@ -16,9 +15,6 @@ done
 for x in `seq 1000`; do
 	if !(curl -s "localhost:$PORT" | diff -q index.html -); then
 		echo "Curl test $x failed"
-		kill "$PID"
 		exit 1
 	fi
 done
-
-kill "$PID"
